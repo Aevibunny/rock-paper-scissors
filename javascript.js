d553c0dfe151e8746fa033c93def3f6ce1a2f20b
Aevibunny <137563523+Aevibunny@users.noreply.github.com>
2023-10-03 13:45:32 -0700

    Finish functionality, alerts winner, add playagain

--- javascript.js
+++ javascript.js
@@ -16,71 +16,82 @@ const getComputerChoice= () => {
     }
 }
 
+const choices = document.querySelector('#choices')
 const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
 const alertBelow = document.querySelector('#alert');
 const playerWinDisplay = document.querySelector('#playerWinDisplay')
 const computerWinDisplay = document.querySelector('#computerWinDisplay')
+const winAlert = document.querySelector('#winAlert');
+const playAgainButton = document.createElement('button');
+playAgainButton.textContent = 'Play Again';
 
 
 const playRound = (playerSelection, computerSelection) => {
     computerSelection = getComputerChoice();
-    if (computerSelection == "rock" && playerSelection == "paper") {
+     if (computerSelection == "rock" && playerSelection == "paper") {
         alertBelow.textContent = "You win! Paper beats Rock";
-        playerWinDisplay.textContent = `${playerWin}`;
-        return ++playerWin;
+        ++playerWin;
     } else if (computerSelection == "rock" && playerSelection == "scissors") {
         alertBelow.textContent = "You lose! Rock beats Scissors";
-        computerWinDisplay.textContent = `${computerWin}`;
-        return ++computerWin;
+        ++computerWin;
     } else if (computerSelection == "paper" && playerSelection == "rock") {
         alertBelow.textContent = "You lose! Paper beats Rock";
-        computerWinDisplay.textContent = `${computerWin}`;
-        return ++computerWin;
+        ++computerWin;
     } else if (computerSelection == "paper" && playerSelection == "scissors") {
         alertBelow.textContent = "You win! Scissors beat Paper";
-        playerWinDisplay.textContent = `${playerWin}`;
-        return ++playerWin;
+        ++playerWin;
     } else if (computerSelection == "scissors" && playerSelection == "rock") {
         alertBelow.textContent = "You win! Rock beats Scissors";
-        playerWinDisplay.textContent = `${playerWin}`;
-        return ++playerWin;
+        ++playerWin;
     } else if (computerSelection == "scissors" && playerSelection == "paper") {
         alertBelow.textContent = "You lose! Scissors beat Paper";
-        computerWinDisplay.textContent = `${computerWin}`;
-        return ++computerWin;
+        ++computerWin;
     } else {
         alertBelow.textContent = 'Tie!';
     };
-}
+};
+
+playAgainButton.addEventListener ('click', () => {
+    location.reload();
+})
 
 rock.addEventListener('click', function() {
     playRound('rock', );
+    playerWinDisplay.textContent = `${playerWin}`;
+    computerWinDisplay.textContent = `${computerWin}`;
+        if (playerWin == 5) {
+        winAlert.textContent = 'You got 5 points, you win!'
+        choices.replaceChildren(playAgainButton);
+    } else if (computerWin == 5) {
+        winAlert.textContent = 'Computer got 5 points, you lose!'
+        choices.replaceChildren(playAgainButton);
+    }
 });
 
 paper.addEventListener('click', function() {
     playRound('paper', );
+    playerWinDisplay.textContent = `${playerWin}`;
+    computerWinDisplay.textContent = `${computerWin}`;
+        if (playerWin == 5) {
+        winAlert.textContent = 'You got 5 points, you win!'
+        choices.replaceChildren(playAgainButton);
+    } else if (computerWin == 5) {
+        winAlert.textContent = 'Computer got 5 points, you lose!'
+        choices.replaceChildren(playAgainButton);
+    }
 });
 
 scissors.addEventListener('click', function() {
     playRound('scissors', );
+    playerWinDisplay.textContent = `${playerWin}`;
+    computerWinDisplay.textContent = `${computerWin}`;
+        if (playerWin == 5) {
+        winAlert.textContent = 'You got 5 points, you win!'
+        choices.replaceChildren(playAgainButton);
+    } else if (computerWin == 5) {
+        winAlert.textContent = 'Computer got 5 points, you lose!'
+        choices.replaceChildren(playAgainButton);
+    }
 });
-
-
-
-
-/* Old play game loop
-let playGame = () => {
-    for (let i = 0; i < 5; i++) {
-        playRound();
-    }
-    if (playerWin > computerWin) {
-        alert('You won best out of 5!');
-    } else if (computerWin > playerWin) {
-        alert('Too bad, computer won best out of 5.');
-    } else if (computerWin === playerWin) {
-        alert('Tie game out of 5')
-    }
-}
-*/
